bc9da04fc4e80a5c55c603fdced03cf685358144
Ismail <forwork.ismail@gmail.com>
2022-06-09 22:34:42 +0430

    Create utils/colors file and add colors to App.tsx

--- App.tsx
+++ App.tsx
@@ -7,11 +7,12 @@ import {
   Text,
   View,
 } from 'react-native';
+import { colors } from './src/utils/colors';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.container}>Hello World</Text>
     </SafeAreaView>
   );
 }
@@ -20,5 +21,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+  text: {
+    color: colors.white,
   },
 });

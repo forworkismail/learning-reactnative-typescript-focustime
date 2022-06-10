86a24e0dd85c9e494e1b3cd9ad6a1a165b9b44cf
Ismail <forwork.ismail@gmail.com>
2022-06-10 18:35:36 +0430

    Create rounded button component

---
+++ src/components/RoundedButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { colors } from '../utils/colors';
+
+export const RoundedButton = ({
+  style = {},
+  textStyle = {},
+  size = 125,
+  ...props
+}) => {
+  return (
+    <TouchableOpacity
+      style={[styles(size).radius, style]}
+      onPress={props.onPress}
+    >
+      <Text style={[styles(size).text, textStyle]}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = (size: number) => ({
+  radius: {
+    borderRadius: size / 2,
+    width: size,
+    height: size,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: colors.white,
+    fontSize: size / 3,
+  },
+});

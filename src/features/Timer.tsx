1ac7b669e46ebc05fcd52b5652d4104168444c6e
Ismail <forwork.ismail@gmail.com>
2022-06-11 11:49:32 +0430

    Add progress bar in Timer feature

--- src/features/Timer.tsx
+++ src/features/Timer.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { ProgressBar } from 'react-native-paper';
 import { CountDown } from '../components/CountDown';
 import { RoundedButton } from '../components/RoundedButton';
+import { colors } from '../utils/colors';
+import { spacing } from '../utils/sizes';
 
 type TimerProps = {
   focusSubject: string;
@@ -11,14 +14,26 @@ type TimerProps = {
 
 const Timer: React.FC<TimerProps> = ({ focusSubject }) => {
   const [isStarted, setIsStarted] = useState(false);
+  const [progress, setProgress] = useState(1);
 
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
         <CountDown
           isPaused={!isStarted}
-          onProgress={() => {}}
+          onProgress={setProgress}
           onEnd={() => {}}
+        />
+        <View style={{ paddingTop: spacing.xxl }}>
+          <Text style={styles.title}>Focusing on:</Text>
+          <Text style={styles.task}>test</Text>
+        </View>
+      </View>
+      <View style={{ paddingTop: spacing.sm }}>
+        <ProgressBar
+          progress={progress}
+          color={colors.progressBar}
+          style={{ height: spacing.sm }}
         />
       </View>
       <View style={styles.buttonWrapper}>
@@ -58,6 +73,15 @@ const styles = StyleSheet.create({
     padding: 15,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  title: {
+    color: colors.white,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  task: {
+    color: colors.white,
+    textAlign: 'center',
   },
 });
 
